f2864a035bc180747a5eec44e2210f42df335734
Matteo Lecca <matteolecca@MacBookdiMatteo.home-life.hub>
2021-04-17 11:19:38 +0200

    first commit

--- src/UI/Spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './Spinner.module.css'
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-const Spinner = props => {
-    const big = props.big ? classes.big : null
-    return <CircularProgress className={[classes.spinner, big].join(' ')}/>
-};
-export default Spinner;
